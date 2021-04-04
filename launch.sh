54e1e26d03e320b972aaacfa5a6c40b94029e6d2
Edward Rodriguez <edwardR36@hotmail.com>
2021-04-04 15:37:22 -0400

    update script file

--- launch.sh
+++ launch.sh
@@ -1,26 +1,16 @@
 # script to launch app to local spark cluster on docker
-# set -e
+# and copy necessary jar files to local spark cluster on docker
 
-# create fat jar
 # sbt assembly
 sbt package
 
-# file to set credentials
-# source keys.sh
-
-
 # this copies the jar files into spark/jars folder
-docker cp lib/aws-java-sdk-bundle-1.11.991.jar spark-master:/spark/jars/aws-java-sdk-bundle-1.11.991.jar
 docker cp lib/config-1.4.1.jar spark-master:/spark/jars/config-1.4.1.jar
 
-# copy jar to docker
+# copy application jar to docker
 docker cp target/scala-2.12/twitter-analytics_2.12-0.1.jar spark-master:/spark/jars/twitter-analytics_2.12-0.1.jar
 
 # submit spark job locally:
-# docker exec -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY spark-master bash -c "./spark/bin/spark-submit --class "HashtagCounterApp" --master local[*] /spark/jars/twitter-analytics-assembly-fatjar-1.0.jar"
-# docker exec spark-master bash -c "./spark/bin/spark-submit --class "HashtagCounterApp" --master local[*] /spark/jars/twitter-analytics-assembly-fatjar-1.0.jar"
-
 docker exec spark-master bash -c "./spark/bin/spark-submit --class "HashtagCounterApp" --master local[*] /spark/jars/twitter-analytics_2.12-0.1.jar"
-# docker exec -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY spark-master bash -c "./spark/bin/spark-submit --class "HashtagCounterApp" --master local[*] /spark/jars/twitter-analytics_2.12-0.1.jar"
 
 
