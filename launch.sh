c5bdc06990c57c52ccff3c310e353adb97500cf1
Edward Rodriguez <edwardR36@hotmail.com>
2021-04-15 15:55:24 -0400

    libraries

--- launch.sh
+++ launch.sh
@@ -6,13 +6,18 @@ sbt package
 
 # this copies the jar files into spark/jars folder
 # only copy over if not in spark/jars folder on cluster
-# docker cp lib/config-1.4.1.jar spark-master:/spark/jars/config-1.4.1.jar
-# docker cp lib/aws-java-sdk-bundle-1.11.991.jar spark-master:/spark/jars/aws-java-sdk-bundle-1.11.991.jar
+ docker cp lib/config-1.4.1.jar spark-master:/spark/jars/config-1.4.1.jar
+ docker cp lib/aws-java-sdk-bundle-1.11.991.jar spark-master:/spark/jars/aws-java-sdk-bundle-1.11.991.jar
+ docker cp lib/hadoop-aws-3.2.0.jar spark-master:/spark/jars/hadoop-aws-3.2.0.jar
+
+# copy over sample file to test
+# docker cp src/main/resources/datalake/30.json.bz2 spark-master:/spark/data/batch/30.json.bz2
 
 # copy application jar to docker
 docker cp target/scala-2.12/twitter-analytics_2.12-0.1.jar spark-master:/spark/jars/twitter-analytics_2.12-0.1.jar
 
 # submit spark job locally (spark cluster on docker)
-docker exec spark-master bash -c "./spark/bin/spark-submit --class "TwitterAnalyticsApp" --master local[*] /spark/jars/twitter-analytics_2.12-0.1.jar"
+docker exec spark-master bash -c "./spark/bin/spark-submit --class "TwitterAnalyticsApp" --master spark://8e98d736f6ec:7077 /spark/jars/twitter-analytics_2.12-0.1.jar"
 
-
+# url to data 
+# https://archive.org/download/archiveteam-twitter-stream-2020-11/twitter-stream-2020-11-02.zip
--- src/main/scala/TwitterAnalyticsApp.scala
+++ src/main/scala/TwitterAnalyticsApp.scala
@@ -11,7 +11,7 @@ object TwitterAnalyticsApp extends App {
   val spark = SparkSession
     .builder()
     .appName("Twitter-Analytics")
-    .master("local[*]")
+    // .master("local[*]")
     .getOrCreate()
 
   // set log level
@@ -23,6 +23,9 @@ object TwitterAnalyticsApp extends App {
   // Path to data in datalake folder on s3
   val inputData = settings.dataLake + settings.inputFile
 
+  // local test file
+  // val inputData = "spark/data/batch/30.json.bz2"
+
   // load custom schema for hashtags
   val hashtagUtil = HashtagsUtil
   val hashtagSchema = hashtagUtil.loadHashtagSchema()
@@ -30,7 +33,7 @@ object TwitterAnalyticsApp extends App {
   // Load file from s3 and convert to df (only select hashtags field)
   val df = spark.read
     .format("json")
-    .option("mode", "FAILFAST")
+    .option("mode", "PERMISSIVE")
     .schema(hashtagSchema)
     .load(inputData)
     .select(explode(col("entities.hashtags.text")) as "hashtags")
